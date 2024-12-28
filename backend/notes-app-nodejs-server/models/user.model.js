3d895d5d15d8d6912e377af0ac4270ec930283e1
ngductoan0712 <ngductoan0712@gmail.com>
2024-12-28 23:04:38 +0700

    add backend version 1

--- backend/notes-app-nodejs-server/models/user.model.js
+++ backend/notes-app-nodejs-server/models/user.model.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-	fullName: { type: String },
-	email: { type: String },
-	password: { type: String },
-	createOn: { type: Date, default: new Date().getTime() },
+    fullName: { type: String },
+    email: { type: String },
+    password: { type: String },
+    createOn: { type: Date, default: new Date().getTime() },
 });
 
 module.exports = mongoose.model("User", userSchema);
